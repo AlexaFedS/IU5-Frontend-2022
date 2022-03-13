adb9e996f3edf5a4b51ca554a5b1f859eb5cb1ad
Alexa <asyasevin@gmail.com>
2022-03-13 18:43:11 +0300

    я сделал 3 лабу

--- lab3/1.js
+++ lab3/1.js
@@ -6,7 +6,15 @@
  * 'я Вижу солнце' -> 'Я Вижу Солнце'
  */
 function capitalize(str) {
-    // code here
+    let a = ""
+    for(let i =0; i< str.length; i++){
+       if(str[i-1]===" " || i === 0){
+           a += str[i].toUpperCase();
+       } else {
+           a +=str[i];
+       }
+    }
+    return a;
 }
 
 module.exports = capitalize;
